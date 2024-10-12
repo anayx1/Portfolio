41a91b27c0960e369478ae9560f72382b0b2b8f2
Anay Tiwari <42086626+anayx1@users.noreply.github.com>
2024-10-12 23:57:22 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ repositories }) {
   return (
     <ContainerBlock
       title="Anay Tiwari - Developer"
-      description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
+      description=""
     >
       <Hero />
       <FavouriteProjects />
